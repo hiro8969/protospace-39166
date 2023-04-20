bd89ba0d84335ca59bd845f6e68e6274e953b6a2
hiro8969 <129718390+hiro8969@users.noreply.github.com>
2023-04-21 07:54:59 +0900

    投稿者下部ページ再修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @prototypes = current_user.prototypes
-  end
+    # @prototypes = current_user.prototypes
+    @prototypes = Prototype.find(params[:id])
 end
