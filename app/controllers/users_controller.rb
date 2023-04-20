dd0d686fe43acb62666d4d83dce958f2c5e416d1
hiro8969 <129718390+hiro8969@users.noreply.github.com>
2023-04-21 07:13:28 +0900

    投稿者詳細ページのプロトタイプ一覧反映箇所修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @prototypes = current_user.prototypes
+    @prototype = Prototype.find(params[:id])
   end
 end
