0aa4b04c73ee758f6b7dbda03700ea2141bb7158
hiro8969 <129718390+hiro8969@users.noreply.github.com>
2023-04-21 07:26:42 +0900

    詳細ページの下部を元に戻した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,5 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @prototypes = current_user.prototypes
-    @prototype = Prototype.find(params[:id])
   end
 end
