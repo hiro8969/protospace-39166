cc58971216e462c2bbee23af375a253cb0eecac3
hiro8969 <129718390+hiro8969@users.noreply.github.com>
2023-04-21 13:26:39 +0900

    Update prototype.rb

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,9 +1,8 @@
 class Prototype < ApplicationRecord
   #dependmentオプションでcommentも削除するよう紐付けた
+  validates :title, :catch_copy, :concept, :image, presence: true
+  
   has_many :comments, dependent: :destroy
   belongs_to :user
   has_one_attached :image
-
-  validates :title, :catch_copy, :concept, :image, presence: true
-
 end
